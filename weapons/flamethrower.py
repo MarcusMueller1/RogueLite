88032ea3f33c5b1e687046c682849750e617835b
Don-Ningo <121505203+Don-Ningo@users.noreply.github.com>
2024-08-15 10:26:30 +0200

    0.2

--- weapons/flamethrower.py
+++ weapons/flamethrower.py
@@ -3,4 +3,4 @@ from weapons.weapon import Weapon
 
 class Flamethrower(Weapon):
     def __init__(self):
-        super().__init__(name="Flamethrower", base_damage=2, fire_rate=80, projectile_speed=4, shape='square', color=(255, 0, 255))
+        super().__init__(name="Flamethrower", base_damage=1.75, fire_rate=80, projectile_speed=4, shape='square', color=(255, 0, 255))
